31b219c650de7c79377b001619f8065e0c0df880
glucciani <giancarlolucciani@gmail.com>
2024-08-01 16:03:12 -0300

    feat: add repository author

--- src/authors/authors.module.ts
+++ src/authors/authors.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { AuthorsResolver } from './graphql/resolvers/authors/authors.resolver';
+import { PrismaService } from '@/database/prisma/prisma.service';
+import { DatabaseModule } from '@/database/database.module';
+import { AuthorsPrismaRepository } from './repositories/authors-prisma.repository';
 
-@Module({})
+@Module({
+  imports: [DatabaseModule],
+  providers: [
+    AuthorsResolver,
+    {
+      provide: 'PrismaService',
+      useClass: PrismaService,
+    },
+    {
+      provide: 'AuthorsRepository',
+      useFactory: (prisma: PrismaService) => {
+        return new AuthorsPrismaRepository(prisma);
+      },
+      inject: ['PrismaService'],
+    },
+  ],
+})
 export class AuthorsModule {}
---
+++ src/authors/interfaces/create-author.ts
@@ -0,0 +1,4 @@
+﻿export interface ICreateAuthor {
+  name: string;
+  email: string;
+}
